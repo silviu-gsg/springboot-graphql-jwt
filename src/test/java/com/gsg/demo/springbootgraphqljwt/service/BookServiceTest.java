1dfa0d279fb847f969a47bd0259d166dc96bc49a
Silviu Gheorghe <silviu.gheorghe@tremend.com>
2021-03-01 22:46:39 +0200

    adjust tests

--- src/test/java/com/gsg/demo/springbootgraphqljwt/service/BookServiceTest.java
+++ src/test/java/com/gsg/demo/springbootgraphqljwt/service/BookServiceTest.java
@@ -2,6 +2,9 @@ package com.gsg.demo.springbootgraphqljwt.service;
 
 import com.gsg.demo.springbootgraphqljwt.entity.Book;
 import com.gsg.demo.springbootgraphqljwt.repository.BookRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -19,14 +22,26 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class BookServiceTest {
 
+    public static final String GRAPHQL_URL = "/graphql";
+
     @Autowired
     MockMvc mockMvc;
 
     @Autowired
     BookRepository bookRepository;
 
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+        bookRepository.deleteAll();
+    }
+
     @Test
-    void persistBooks() throws Exception {
+    @Order(0)
+    void addOneBook() throws Exception {
 
         Book bookToSave = Book.builder()
                 .isbn("123-456-789")
@@ -37,15 +52,70 @@ class BookServiceTest {
                 .build();
         bookRepository.save(bookToSave);
 
-        String expectedResponse = "{\"data\":{\"books\":[{\"id\":1,\"isbn\":\"123-456-789\",\"title\":\"first title\"," +
+        String booksQuery = "{\"query\":\"{ books { id isbn title author publisher publishedAt } }\"}";
+
+        String expectedResponse = "{\"data\":{\"books\":[{\"id\":2,\"isbn\":\"123-456-789\",\"title\":\"first title\"," +
                 "\"author\":\"first author\",\"publisher\":\"first publisher\",\"publishedAt\":\"2020-12-20\"}]}}";
 
-        mockMvc.perform(post("/graphql")
-                .content("{\"query\":\"{ books { id isbn title author publisher publishedAt } }\"}")
+        mockMvc.perform(post(GRAPHQL_URL)
+                .content(booksQuery)
                 .contentType(APPLICATION_JSON)
                 .accept(APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().json(expectedResponse))
+                .andReturn();
+    }
+
+    @Test
+    @Order(1)
+    void addAndRemoveBook() throws Exception {
+        String createBookMutation = "{\"query\":\"mutation { saveBook(book: { isbn: \\\"123-456-789\\\" title: " +
+                "\\\"first title\\\" author: \\\"first author\\\" publisher: \\\"first publisher\\\" publishedAt: " +
+                "\\\"2020-12-20\\\" }) { id isbn title author publisher publishedAt } }\"}";
+
+        String newBookCreatedSuccessfulResponse = "{\"data\":{\"saveBook\":{\"id\":1,\"isbn\":\"123-456-789\"," +
+                "\"title\":\"first title\",\"author\":\"first author\",\"publisher\":\"first publisher\"," +
+                "\"publishedAt\":\"2020-12-20\"}}}";
+
+        String allBooksQuery = "{\"query\":\"{ books { id isbn title } }\"}";
+
+        String noBooksResponse = "{\"data\":{\"books\":[]}}";
+
+        String deleteBookMutation = "{\"query\":\"mutation { deleteBook(id: 1) }\"}";
+
+        // expect new book to not be there initially
+        mockMvc.perform(post(GRAPHQL_URL)
+                .content(allBooksQuery)
+                .contentType(APPLICATION_JSON)
+                .accept(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(noBooksResponse))
+                .andReturn();
+
+        // add new book successfully
+        mockMvc.perform(post(GRAPHQL_URL)
+                .content(createBookMutation)
+                .contentType(APPLICATION_JSON)
+                .accept(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(newBookCreatedSuccessfulResponse))
+                .andReturn();
+
+        // remove new book
+        mockMvc.perform(post(GRAPHQL_URL)
+                .content(deleteBookMutation)
+                .contentType(APPLICATION_JSON)
+                .accept(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        // list book and expect new book to not be there
+        mockMvc.perform(post(GRAPHQL_URL)
+                .content(allBooksQuery)
+                .contentType(APPLICATION_JSON)
+                .accept(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(noBooksResponse))
                 .andReturn();
     }
 
