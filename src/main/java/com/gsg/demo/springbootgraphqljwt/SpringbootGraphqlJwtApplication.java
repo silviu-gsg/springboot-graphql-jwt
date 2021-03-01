0c96f16dae7e6f5ad7b33374b48327a043d23640
Silviu Gheorghe <silviu.gheorghe@tremend.com>
2021-03-01 21:20:39 +0200

    initial commit

---
+++ src/main/java/com/gsg/demo/springbootgraphqljwt/SpringbootGraphqlJwtApplication.java
@@ -0,0 +1,13 @@
+package com.gsg.demo.springbootgraphqljwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGraphqlJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGraphqlJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsg/demo/springbootgraphqljwt/SpringbootGraphqlJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsg.demo.springbootgraphqljwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGraphqlJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
